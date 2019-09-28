78ce9752302b4b16ed466b91ae237a81b2a47f7e
Gavin Lange <gavincarllange@gmail.com>
2019-09-28 11:48:15 -0500

    Updated ICE_Controls through Vuforia Trackables

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ICE_Controls.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ICE_Controls.java
@@ -2,7 +2,6 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -47,6 +46,7 @@ public abstract class ICE_Controls<TODO> extends LinearOpMode {
     OpenGLMatrix lastLocation = null;
     private VuforiaLocalizer vuforia;
     private static final String VUFORIA_KEY = "AZWzerv/////AAABmZeKo4MkD08MoSz5oHB/JU6N1BsUWpfHgQeAeVZemAypSUGVQhvAHo6+v7kJ3MITd8530MhwxRx7GjRtdCs1qjPmdKiJK66dv0yN4Zh4NvKBfP5p4TJjM+G0GoMVgVK0pItm2U56/SVqQH2AYtczQ+giw6zBe4eNhHPJCMY5C2t5Cs6IxxjZlMkRF85l8YAUlKGLipnoZ1T/mX8DNuThQA57qsIB2EN6pGWe8GI64hcPItQ0j7Oyjp82lEN13rYQYsS3Ur4a6//D6yhwa0rogXAysG68G+VgC1mNlj1CjX60qDI84ZN0b/A081xXqjeyFqZK8A/jO8y7BGz9ZuuZNxxXIon6xRNeKYudpfTD23+5";
+
     /**
      * {@link #tfod} is the variable we will use to store our instance of the Tensor Flow Object
      * Detection engine.
@@ -55,6 +55,7 @@ public abstract class ICE_Controls<TODO> extends LinearOpMode {
     private static final String TFOD_MODEL_ASSET = "Skystone.tflite";
     private static final String LABEL_FIRST_ELEMENT = "Stone";
     private static final String LABEL_SECOND_ELEMENT = "Skystone";
+
 
     //Initialize elapsed time object
     private ElapsedTime runtime = new ElapsedTime();
@@ -68,6 +69,38 @@ public abstract class ICE_Controls<TODO> extends LinearOpMode {
     protected Orientation angles;
     protected Acceleration gravity;
 
+    //ROBOT HARDWARE
+    //Instantiate chassis motors
+
+    protected DcMotorEx leftMotor;
+    protected DcMotorEx rightMotor;
+
+    //Instantiate servos
+    //protected Servo ????;
+
+    //Instantiate sensors
+    //ColorSensor ?????;
+
+    //Initlize encoder variables
+    protected double COUNTS_PER_MOTOR_DRIVE = 1120;    // Rev Hex Motor 288
+    protected double COUNTS_PER_MOTOR_LIFT = 1120;    // andyMark 40
+    protected double DRIVE_GEAR_REDUCTION = 1;     // This is < 1.0 if geared UP
+    protected double WHEEL_DIAMETER_INCHES = 3.5;     // For figuring circumference
+    protected double COUNTS_PER_INCH = (COUNTS_PER_MOTOR_DRIVE * DRIVE_GEAR_REDUCTION) / (WHEEL_DIAMETER_INCHES * 3.1415);
+    protected double COUNTS_PER_INCH_LIFT = (COUNTS_PER_MOTOR_LIFT) / (.677 * 3.1415);
+
+    // These constants define the desired driving/control characteristics
+    // The can/should be tweaked to suite the specific robot drive train.
+    static final double DRIVE_SPEED = .3;     // Nominal speed for better accuracy.
+    static final double TURN_SPEED = .3;     // Nominal half speed for better accuracy.
+
+    static final double HEADING_THRESHOLD = 2.5;      // As tight as we can make it with an integer gyro
+    static final double P_TURN_COEFF = .010;     // .02 Larger is more responsive, but also less stable
+    static final double P_DRIVE_COEFF = .010;     // .05 Larger is more responsive, but also less stable
+
+    //Initialize Vuforia variables
+    //VuforiaTrackables relicTrackables;
+    //VuforiaTrackable relicTemplate;
 
     //TODO Add public methods here ie. public void gyroDrive
 
